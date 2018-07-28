dda661617725bc7807e2f18d33cf1f860d4e3ee9
Steve Oh <OhxSteve@gmail.com>
2018-07-28 00:23:19 -0700

    Implement Hover

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -6,15 +6,26 @@ class Photo extends React.Component {
     super(props);
     this.state = {
       clicked:false,
+      hovered:false,
     };
+  }
+
+  handleHover = () => {
+    this.setState({
+      hovered:!this.state.hovered
+    })
   }
 
   render() {
     return (
-      <div className="photo">
+
+      <div className="photo" onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
         <img src={this.props.photo.photo} alt="shoe" height="612" width="612" />
         <div>{this.props.photo.user}</div>
         <div>{this.props.photo.likes} Likes</div>
+        {this.state.hovered && 
+        <div className="shop">Shop the Look</div>
+        }
       </div>
     );
   }
