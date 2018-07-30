17035b8925c392d7d56dd2210b3c6f87c1440367
Steve Oh <OhxSteve@gmail.com>
2018-07-29 21:49:51 -0700

    Refactor seeding

--- generate.js
+++ generate.js
@@ -5,7 +5,7 @@ const { connection } = require('./db');
 
 const out = fs.createWriteStream('/tmp/shoes.csv');
 const records = Array(500).fill().map((e, i) => {
-  return `https://s3-us-west-1.amazonaws.com/wornshoes/${i % 34}.jpg,@${faker.internet.userName()},${faker.random.number()},${faker.date.past().getTime()},${(i) % 100}`;
+  return `https://s3-us-west-1.amazonaws.com/wornshoes/${i % 31}.jpg,@${faker.internet.userName()},${faker.random.number()},${faker.date.past().getTime()},${(i) % 100}`;
 });
 records.forEach((i) => {
   out.write(`${i}\n`);
